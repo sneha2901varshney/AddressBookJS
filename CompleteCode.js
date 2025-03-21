5595527f39a5f711b3aedd7e6b09b5f9810d4711
snehavarshney <snehavarshney111@gmail.com>
2025-03-21 11:16:47 +0530

    Added:Find and Edit Contact

--- CompleteCode.js
+++ CompleteCode.js
@@ -76,3 +76,42 @@ contactDetails('Kushagra', 'Sharma', 'Home', 'Uttar Pradesh', 'Agra', "282005", 
 console.log(addressBook);
 
 
+//UC4
+
+//method to find and editContacts
+function editContact(findName,editedVariable,variableNewValue){
+    if(addressBook.length == null){
+        console.log("Add Contact In Address Book");
+    }else{
+        addressBook.forEach(newContact => {
+            if(newContact.firstName == findName){
+                switch(editedVariable){
+                    case "firstName":
+                        newContact.firstName = variableNewValue;
+                        break;
+                    case "lastName":
+                        newContact.lastName = variableNewValue;
+                        break;
+                    case "address":
+                        newContact.address = variableNewValue;
+                        break;
+                    case "state":
+                        newContact.state = variableNewValue;
+                        break;
+                    case "city":
+                        newContact.city = variableNewValue;
+                        break;
+                    case "zipCode":
+                        newContact.zipCode = variableNewValue;
+                        break;  
+                    case "firstName":
+                        newContact.firstName = variableNewValue;
+                        break;
+                    case "lastName":
+                        newContact.lastName = variableNewValue;
+                        break;      
+                }
+            }
+        })
+    }
+}

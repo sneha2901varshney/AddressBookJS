a868512a83e348fe35043a726b29325f71259f37
snehavarshney <snehavarshney111@gmail.com>
2025-03-21 11:38:12 +0530

    Added: Search Contact

--- CompleteCode.js
+++ CompleteCode.js
@@ -151,3 +151,23 @@ function isDuplicate(firstName) {
     let duplicateCount = duplicates.reduce((count) => count + 1, 0);
     return duplicateCount > 0;
 }
+
+
+//UC 8
+
+ // method to Search Contact 
+ function searchByCity_State(choice , name){
+    if(choice == "city"){
+        person = addressBook.filter(contact => contact.city == name)
+        .map(contact => contact.firstName);
+        console.log("Contact Found Who Is From "+name);
+        console.log(person);
+    }else if(choice == "state"){
+        person = addressBook.filter(contact => contact.state == name)
+        .map(contact => contact.firstName);
+        console.log("Contact Found Who Is From "+name);
+        console.log(person);
+    }else{
+        console.log("Provide Right City or State Name");
+    }
+}
